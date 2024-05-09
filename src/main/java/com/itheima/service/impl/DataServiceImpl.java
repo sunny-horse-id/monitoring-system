f219833e2469e360e85ec79d7cc4ef6d10e6fd67
zy <1824762582@qq.com>
2024-05-09 14:38:23 +0800

    数据bug的修复

--- src/main/java/com/itheima/service/impl/DataServiceImpl.java
+++ src/main/java/com/itheima/service/impl/DataServiceImpl.java
@@ -13,6 +13,7 @@ import java.util.*;
 public class DataServiceImpl implements DataService {
     @Autowired
     private DataMapper dataMapper;
+
     @Override
     public List<Map<String, Object>> getData() {
         List<Map<String, Object>> res = new ArrayList<>();
@@ -29,13 +30,27 @@ public class DataServiceImpl implements DataService {
         healthMap.put("name", "储能侧健康度/%");
         DecimalFormat df = new DecimalFormat("#.00");
         double randomDecimal = Math.random() * 99 + 1;
+        List<Double> voltageList = new ArrayList<>();
+        List<Double> electricCurrentList = new ArrayList<>();
+        List<Double> pressureList = new ArrayList<>();
+        List<Double> concentrationList = new ArrayList<>();
         for (int i = 0; i < data.size(); i++) {
             Data d = data.get(i);
+            voltageList.add(d.getVoltage());
+            electricCurrentList.add(d.getElectricCurrent());
+            pressureList.add(d.getPressure());
+            concentrationList.add(d.getConcentration());
+        }
+        Collections.shuffle(voltageList);
+        Collections.shuffle(electricCurrentList);
+        Collections.shuffle(pressureList);
+        Collections.shuffle(concentrationList);
+        for (int i = 0; i < data.size(); i++) {
             // 创建一个新的哈希表来存储当前数据点的所有属性
-            voltageMap.put("date_" + i, d.getVoltage());
-            electricCurrentMap.put("date_" + i, d.getElectricCurrent());
-            pressureMap.put("date_" + i, d.getPressure());
-            concentrationMap.put("date_" + i, d.getConcentration());
+            voltageMap.put("date_" + i, voltageList.get(i));
+            electricCurrentMap.put("date_" + i, electricCurrentList.get(i));
+            pressureMap.put("date_" + i, pressureList.get(i));
+            concentrationMap.put("date_" + i, concentrationList.get(i));
             randomDecimal = randomDecimal - Math.random() * 0.1;
             randomDecimal = Double.parseDouble(df.format(Math.max(randomDecimal, 0)));
             healthMap.put("date_" + i, randomDecimal);
